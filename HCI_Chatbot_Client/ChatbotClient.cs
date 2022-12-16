8286c683eacc7a4a237fd18eec12beb6c66ec38e
Andy Ponce <andy.ponce@outlook.com>
2022-12-16 10:45:12 -0500

    Chatbot client is now able to continually input messages (#2)
    Each message does not need <EOF> at the end to be sent

--- HCI_Chatbot_Client/ChatbotClient.cs
+++ HCI_Chatbot_Client/ChatbotClient.cs
@@ -22,60 +22,60 @@ namespace HCI_Chatbot_Client
         public void StartClient()
         {
             byte[] bytes = new byte[1024];
-
-            try
+            while (true)
             {
-                // Connect to a Remote server
-                // Get Host IP Address that is used to establish a connection
-                // In this case, we get one IP address of localhost that is IP : 127.0.0.1
-                // If a host has multiple addresses, you will get a list of addresses
-
-                // Create a TCP/IP  socket.
-                Socket sender = new Socket(ipAddress.AddressFamily,
-                    SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect the socket to the remote endpoint. Catch any errors.
                 try
                 {
-                    // Connect to Remote EndPoint
-                    sender.Connect(remoteEP);
+                    // Connect to a Remote server
+                    // Get Host IP Address that is used to establish a connection
+                    // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+                    // If a host has multiple addresses, you will get a list of addresses
 
-                    Console.WriteLine("Socket connected to {0}",
-                        sender.RemoteEndPoint.ToString());
+                    // Create a TCP/IP  socket.
+                    Socket sender = new Socket(ipAddress.AddressFamily,
+                        SocketType.Stream, ProtocolType.Tcp);
 
-                    // Encode the data string into a byte array.
-                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+                    // Connect the socket to the remote endpoint. Catch any errors.
+                    try
+                    {
+                        // Connect to Remote EndPoint
+                        sender.Connect(remoteEP);
 
-                    // Send the data through the socket.
-                    int bytesSent = sender.Send(msg);
+                        Console.WriteLine("Socket connected to {0}",
+                            sender.RemoteEndPoint.ToString());
+                        // Encode the data string into a byte array.
+                        byte[] msg = Encoding.ASCII.GetBytes(Console.ReadLine().ToString());
 
-                    // Receive the response from the remote device.
-                    int bytesRec = sender.Receive(bytes);
-                    Console.WriteLine("Echoed test = {0}",
-                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                        // Send the data through the socket.
+                        int bytesSent = sender.Send(msg);
 
-                    // Release the socket.
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
+                        // Receive the response from the remote device.
+                        int bytesRec = sender.Receive(bytes);
+                        Console.WriteLine("Echoed test = {0}",
+                            Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                        // Release the socket.
+                        sender.Shutdown(SocketShutdown.Both);
+                        sender.Close();
 
-                }
-                catch (ArgumentNullException ane)
-                {
-                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                }
-                catch (SocketException se)
-                {
-                    Console.WriteLine("SocketException : {0}", se.ToString());
+                    }
+                    catch (ArgumentNullException ane)
+                    {
+                        Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                    }
+                    catch (SocketException se)
+                    {
+                        Console.WriteLine("SocketException : {0}", se.ToString());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                    }
+
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                    Console.WriteLine(e.ToString());
                 }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
             }
         }
     }
